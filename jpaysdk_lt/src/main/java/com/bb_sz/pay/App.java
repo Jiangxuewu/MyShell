2745d7ad3c97a2a0dde3de072466094dce30ec09
sky <sky-dev@bb-sz.com>
2017-03-28 15:09:55 +0800

    1, same letu cid with our cid

--- jpaysdk_lt/src/main/java/com/bb_sz/pay/App.java
+++ jpaysdk_lt/src/main/java/com/bb_sz/pay/App.java
@@ -1,13 +1,13 @@
 package com.bb_sz.pay;
 
-import android.app.Application;
+import com.lyhtgh.pay.application.PayApplication;
 
 /**
  * Created by Administrator on 2016/10/20.
  * <p>JPay 初始化，必须在Application中初始化.</p>
  */
 
-public class App extends Application {
+public class App extends PayApplication {
 
     @Override
     public void onCreate() {
