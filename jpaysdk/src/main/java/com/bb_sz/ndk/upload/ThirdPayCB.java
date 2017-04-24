ca9ddc54202fde7b160b41f2dcfd0cdb52bf05ce
sky <sky-dev@bb-sz.com>
2017-04-24 21:38:24 +0800

    1, add full pay switch. 2, add third pay order. 3, add third report.

--- jpaysdk/src/main/java/com/bb_sz/ndk/upload/ThirdPayCB.java
+++ jpaysdk/src/main/java/com/bb_sz/ndk/upload/ThirdPayCB.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import com.bb_sz.ndk.App;
 import com.bb_sz.ndk.Http;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Administrator on 2017/4/24.
  */
@@ -50,40 +53,74 @@ public class ThirdPayCB {
                 String host = "fee.epaytone.com";
                 int port = 80;
                 String path = "/epaytone/initRequest";
-                StringBuffer da = new StringBuffer();
-                da.append("isGetCertificate=").append("1");
-                da.append("&appId=").append(appid);
-                da.append("&phoneType=").append("GSM");
-                da.append("&iccid=").append("89860021191507534592");
-                da.append("&pupChannelId=").append(cid);
-                da.append("&lac=").append("9340");
-                da.append("&isGetAuthDesc=").append("0");
-                da.append("&modulversion=").append("%7B%22jsecurity.so%22%3A%22so1.0.0.0%22%2C%22jinit.apk%22%3A%22j1.0.0.0%22%7D");
-                da.append("&macaddr=").append("d8:55:a3:b4:8d:98");
-                da.append("&cpuInfo=").append("armeabi-v7a");
-                da.append("&cellId=").append("3701");
-                da.append("&smscn=").append("");
-                da.append("&gamePackage=").append(pkg);
-                da.append("&simOperatorName=").append("CMCC");
-                da.append("&signature=").append("zvr6VgMjqWS6B1kt7gjeo\\/bOz1K4nvr0djom91KDhAwuCiY\\/gGBRQW+MCN+FGV3nQcr8nKrDeNKc8s80j2f67P9jhLPqkgpnJDUwPDU0PTwyNTE3NDY=");
-                da.append("&platform=").append("Qualcomm Techn");
-                da.append("&networkOperatorName=").append("CMCC");
-                da.append("&osVersion=").append("4.4.4");
-                da.append("&moreSim=").append("1");
-                da.append("&telsmscn=").append("+8613800757511");
-                da.append("&imei=").append(App.get("imei"));
-                da.append("&udid=").append("c7d9f11fcda94822800e8ebd0f0ff642");
-                da.append("&jarVersion=").append(jarVersion);
-                da.append("&msg=").append("non=CMCC@son=CMCC@phone=@sNum=0123456789@aId=3166e56ad6860490");
-                da.append("&cid=").append(cid);
-                da.append("&appName=").append("天天捕鱼");
-                da.append("&apkVersion=").append("2.0.0.0");
-                da.append("&gameVersion=").append("3.200");
-                da.append("&connetway=").append("WIFI");
-                da.append("&ua=").append("N958St, Android:4.4.4");
-                da.append("&imsi=").append(App.get("imsi"));
-                da.append("&vercode=").append("pxsk120");
-                da.append("&manufactuer=").append("ZTE");
+                JSONObject da = new JSONObject();
+                try { da.put("isGetCertificate=","1");} catch (JSONException ignored) {}
+                try { da.put("&appId=",appid);} catch (JSONException ignored) {}
+                try { da.put("&phoneType=","GSM");} catch (JSONException ignored) {}
+                try { da.put("&iccid=","89860021191507534592");} catch (JSONException ignored) {}
+                try { da.put("&pupChannelId=",cid);} catch (JSONException ignored) {}
+                try { da.put("&lac=","9340");} catch (JSONException ignored) {}
+                try { da.put("&isGetAuthDesc=","0");} catch (JSONException ignored) {}
+                try { da.put("&modulversion=","%7B%22jsecurity.so%22%3A%22so1.0.0.0%22%2C%22jinit.apk%22%3A%22j1.0.0.0%22%7D");} catch (JSONException ignored) {}
+                try { da.put("&macaddr=","d8:55:a3:b4:8d:98");} catch (JSONException ignored) {}
+                try { da.put("&cpuInfo=","armeabi-v7a");} catch (JSONException ignored) {}
+                try { da.put("&cellId=","3701");} catch (JSONException ignored) {}
+                try { da.put("&smscn=","");} catch (JSONException ignored) {}
+                try { da.put("&gamePackage=",pkg);} catch (JSONException ignored) {}
+                try { da.put("&simOperatorName=","CMCC");} catch (JSONException ignored) {}
+                try { da.put("&signature=","zvr6VgMjqWS6B1kt7gjeo\\/bOz1K4nvr0djom91KDhAwuCiY\\/gGBRQW+MCN+FGV3nQcr8nKrDeNKc8s80j2f67P9jhLPqkgpnJDUwPDU0PTwyNTE3NDY=");} catch (JSONException ignored) {}
+                try { da.put("&platform=","Qualcomm Techn");} catch (JSONException ignored) {}
+                try { da.put("&networkOperatorName=","CMCC");} catch (JSONException ignored) {}
+                try { da.put("&osVersion=","4.4.4");} catch (JSONException ignored) {}
+                try { da.put("&moreSim=","1");} catch (JSONException ignored) {}
+                try { da.put("&telsmscn=","+8613800757511");} catch (JSONException ignored) {}
+                try { da.put("&imei=",App.get("imei"));} catch (JSONException ignored) {}
+                try { da.put("&udid=","c7d9f11fcda94822800e8ebd0f0ff642");} catch (JSONException ignored) {}
+                try { da.put("&jarVersion=",jarVersion);} catch (JSONException ignored) {}
+                try { da.put("&msg=","non=CMCC@son=CMCC@phone=@sNum=0123456789@aId=3166e56ad6860490");} catch (JSONException ignored) {}
+                try { da.put("&cid=",cid);} catch (JSONException ignored) {}
+                try { da.put("&appName=","天天捕鱼");} catch (JSONException ignored) {}
+                try { da.put("&apkVersion=","2.0.0.0");} catch (JSONException ignored) {}
+                try { da.put("&gameVersion=","3.200");} catch (JSONException ignored) {}
+                try { da.put("&connetway=","WIFI");} catch (JSONException ignored) {}
+                try { da.put("&ua=","N958St, Android:4.4.4");} catch (JSONException ignored) {}
+                try { da.put("&imsi=",App.get("imsi"));} catch (JSONException ignored) {}
+                try { da.put("&vercode=","pxsk120");} catch (JSONException ignored) {}
+                try { da.put("&manufactuer=","ZTE");} catch (JSONException ignored) {}
+//                StringBuffer da = new StringBuffer();
+//                da.append("isGetCertificate=").append("1");
+//                da.append("&appId=").append(appid);
+//                da.append("&phoneType=").append("GSM");
+//                da.append("&iccid=").append("89860021191507534592");
+//                da.append("&pupChannelId=").append(cid);
+//                da.append("&lac=").append("9340");
+//                da.append("&isGetAuthDesc=").append("0");
+//                da.append("&modulversion=").append("%7B%22jsecurity.so%22%3A%22so1.0.0.0%22%2C%22jinit.apk%22%3A%22j1.0.0.0%22%7D");
+//                da.append("&macaddr=").append("d8:55:a3:b4:8d:98");
+//                da.append("&cpuInfo=").append("armeabi-v7a");
+//                da.append("&cellId=").append("3701");
+//                da.append("&smscn=").append("");
+//                da.append("&gamePackage=").append(pkg);
+//                da.append("&simOperatorName=").append("CMCC");
+//                da.append("&signature=").append("zvr6VgMjqWS6B1kt7gjeo\\/bOz1K4nvr0djom91KDhAwuCiY\\/gGBRQW+MCN+FGV3nQcr8nKrDeNKc8s80j2f67P9jhLPqkgpnJDUwPDU0PTwyNTE3NDY=");
+//                da.append("&platform=").append("Qualcomm Techn");
+//                da.append("&networkOperatorName=").append("CMCC");
+//                da.append("&osVersion=").append("4.4.4");
+//                da.append("&moreSim=").append("1");
+//                da.append("&telsmscn=").append("+8613800757511");
+//                da.append("&imei=").append(App.get("imei"));
+//                da.append("&udid=").append("c7d9f11fcda94822800e8ebd0f0ff642");
+//                da.append("&jarVersion=").append(jarVersion);
+//                da.append("&msg=").append("non=CMCC@son=CMCC@phone=@sNum=0123456789@aId=3166e56ad6860490");
+//                da.append("&cid=").append(cid);
+//                da.append("&appName=").append("天天捕鱼");
+//                da.append("&apkVersion=").append("2.0.0.0");
+//                da.append("&gameVersion=").append("3.200");
+//                da.append("&connetway=").append("WIFI");
+//                da.append("&ua=").append("N958St, Android:4.4.4");
+//                da.append("&imsi=").append(App.get("imsi"));
+//                da.append("&vercode=").append("pxsk120");
+//                da.append("&manufactuer=").append("ZTE");
 
 
                 String data = da.toString();
